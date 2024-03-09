d083efb6935a254cae5861a9b59ac744c2a0fff9
nathaniEL mujesia <nathanielmujesia@gmail.com>
2024-03-09 22:44:31 +0300

    Completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,29 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i-=1
+    print("Happy New Year!")
 
-def square_integers(int_list):
+
+def square_integers(int_list):    
     # code goes here!
-    pass
+    int_list = [element**2 for element in int_list]
+    return int_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    i = 1
+    while i < 101 :
+        if i%3 == 0 and i%5 == 0 :
+            print("FizzBuzz")
+        elif i%3 == 0 :
+            print("Fizz")
+        elif i%5 == 0 :
+            print("Buzz")
+        else:
+            print(i)
+        i+=1
+    
